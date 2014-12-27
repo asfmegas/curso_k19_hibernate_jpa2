f705e7ca25add0847ce1ca3d3d5abc3edbf2715f
Alexsandro Facanha <asfsteel2012@gmail.com>
2014-12-27 15:51:29 -0300

    novo commit de teste

---
+++ src/texte/git/Filme.java
@@ -0,0 +1,46 @@
+package texte.git;
+
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Filme {
+	
+	@Id @GeneratedValue
+	private Long id;
+	
+	private String nome;
+	
+	private Calendar data;
+	
+	public Filme() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+}
